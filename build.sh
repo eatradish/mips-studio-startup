3d0064a19a92b4db4945c1bbe0375f319dec2d2a
eatradish <sakiiily@aosc.io>
2023-10-09 15:03:58 +0800

    chore: add build script

---
+++ build.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+DIR="$(pwd)"
+
+wget https://mirrors.bfsu.edu.cn/github-release/VSCodium/vscodium/LatestRelease/VSCodium-linux-x64-1.83.0.23277.tar.gz \
+	-O vscodium.tar.gz
+mkdir -pv "$DIR"/resources/vscodium
+cd "$DIR"/resources/vscodium
+tar xvf "$DIR"/vscodium.tar.gz
+mkdir -pv "$DIR"/resources/vscodium/data
+cd "$DIR"
+npm run electron:build
+
+
+
